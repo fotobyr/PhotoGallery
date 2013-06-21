ee0fd1ba649b29fb8ded21df76fcf7528e4eb9a2
gengzu <gengzu@gmail.com>
2013-06-21 12:15:15 -0700

    add simple angular example

--- routes/user.js
+++ routes/user.js
@@ -6,3 +6,7 @@
 exports.list = function(req, res){
   res.send("respond with a resource");
 };
+
+exports.details = function(req, res){
+	res.send('details for user with id: ' + req.params.userId);
+}
