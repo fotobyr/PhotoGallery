4a100977e58a360860e606871f78e56d386e4e38
gengzu <gengzu@gmail.com>
2013-06-21 12:23:03 -0700

    add new files

---
+++ public/javascripts/photoGallery.js
@@ -0,0 +1,7 @@
+function galleryContoller($scope){
+	$scope.photos = [
+		{ name: 'photo 1', src: '/images/1.png', desc: 'description 111'},
+		{ name: 'photo 2', src: '/images/2.png', desc: 'description 222'},
+		{ name: 'photo 3', src: '/images/3.png', desc: 'description 333'},
+	];
+}
---
+++ test/test.js
@@ -0,0 +1,18 @@
+var assert = require('assert'),
+	should = require('should');
+
+describe("Photo", function(){
+	describe("#comments", function(){
+		it('should return all commens for a photo', function(){
+			'5'.should.equal('5');
+		})
+	})
+})
+
+describe("Photo", function(){
+	describe("#addComment", function(){
+		it('should return all commens for a photo', function(){
+			'5'.should.equal('5');
+		})
+	})
+})
