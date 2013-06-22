24f34d84f1ac6c6117f05144a5c9c22b0f4ac638
gengzu <gengzu@gmail.com>
2013-06-22 13:21:25 +0300

    rename controller

--- public/javascripts/photoGallery.js
+++ public/javascripts/photoGallery.js
@@ -2,7 +2,7 @@ var photoGalleryApp = angular.module('galleryApp', []);
 
 photoGalleryApp.config(function($routeProvider){
 	$routeProvider
-		.when('/', {controller: galleryContoller, templateUrl: '/html/photos.html'})
+		.when('/', {controller: galleryCtrl, templateUrl: '/html/photos.html'})
 		.when('/photo', {controller: photoDetailsCtrl, templateUrl: '/html/details.html'})
 		.otherwise({redirectTo: '/'});
 });
@@ -11,10 +11,11 @@ function photoDetailsCtrl($scope){
 	$scope.mf = 'ebana';
 }
 
-function galleryContoller($scope){
+function galleryCtrl($scope){
 	$scope.photos = [
 		{ name: 'photo 1', src: '/images/1.png', desc: 'description 111'},
 		{ name: 'photo 2', src: '/images/2.png', desc: 'description 222'},
 		{ name: 'photo 3', src: '/images/3.png', desc: 'description 333'},
+        { name: 'photo 666', src: '/images/3.png', desc: 'description 333'},
 	];
 }
