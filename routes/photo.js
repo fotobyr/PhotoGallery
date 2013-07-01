998f1c5ac16373989a5825751530e13af951e1f1
gengzu <gengzu@gmail.com>
2013-07-01 15:17:36 +0300

    add more magic. use mongoDB.

--- routes/photo.js
+++ routes/photo.js
@@ -6,25 +6,25 @@
  * To change this template use File | Settings | File Templates.
  */
 
+//var db = require('monk')('mongodb://gengzu:leeloo123@ds027758.mongolab.com:27758/PhotoGallery-Mongo'),
+var db = require('monk')('localhost/gallery'),
+    photos = db.get('photos');
+
 exports.list = function(req, res){
-
-    var photos = [
-        {id: 1, name: 'photo 1', desc: 'desc 1'},
-        {id: 22, name: 'photo 22', desc: 'desc 2'},
-        {id: 333, name: 'photo 333', desc: 'desc 3'},
-        {id: 666, name: 'photo 666', desc: 'desc 6'}
-    ];
-
-    res.json(photos)
+    photos.find({}, function(err, docs){
+        res.json(docs);
+    });
 }
 
 exports.get = function(req, res){
+    photos.findById(req.params.photoId, function(err, doc){
+        res.json(doc);
+    })
+}
 
-    var photo = {
-        id: req.params.photoId,
-        name: 'requested id# ' + req.params.photoId,
-        comments: null
-    };
-
-    res.json(photo);
+function populatePhotos(){
+    photos.insert({name: 'photo 22', desc: 'desc 2'});
+    photos.insert({name: 'photo 333', desc: 'desc 3'});
+    photos.insert({name: 'photo 666', desc: 'desc 6'});
+    photos.insert({name: 'photo 1', desc: 'desc 1'});
 }
