ad73e4fefec9ec9be5d3110925cf0ac6d1e2f3de
gengzu <gengzu@outlook.com>
2013-11-16 16:36:16 +0200

    migrate one test to jasmine.

--- spec/userService-spec.js
+++ spec/userService-spec.js
@@ -1,26 +1,28 @@
-var assert = require('assert'),
-	should = require('should'),
-    userService = require('../lib/userService').create();
+/**
+ * Created by Oleg on 16.11.13.
+ */
 
-describe("UserService", function(){
+userService = require('../lib/userService').create();
+
+describe("User service", function(){
     it('password should not be empty', function(){
-        userService.validate({
+        expect(userService.validate({
             password: '',
             repeatPassword: ''
-        }).should.false;
+        })).toBe(false);
     })
 
     it('repeatPassword should be same as password', function(){
-        userService.validate({
+        expect(userService.validate({
             password: '111',
             repeatPassword: '222'
-        }).should.false;
+        })).toBe(false);
     })
 
     it('should return true if all is ok', function(){
-        userService.validate({
+        expect(userService.validate({
             password: '666',
             repeatPassword: '666'
-        }).should.true;
+        })).toBe(true);
     })
 })
